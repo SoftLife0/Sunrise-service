d0807dbb99a68b2e02e0bac2f4a1a93db5be4dc0
SoftLife0 <onikosiadewale18@gmail.com>
2025-07-12 11:00:40 +0000

    Update Footer component styling and logo; enhance copyright notice for improved clarity

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import logo from '../assets/s.png';
+import logo from '../assets/s3.png';
 
 const Footer = () => {
   return (
-    <footer className="py-5  text-white text-center" style={{background: "#008080"}}> 
+    <footer className="py-5  text-gray bg-white text-center"> 
       <div className="container">
         <div className="mx-auto text-center d-flex justify-content-center align-items-center">
           <Link to="/admin/login">
@@ -12,8 +12,7 @@ const Footer = () => {
           </Link>
         </div>
         <hr />     
-        <small className="mb-3">© {new Date().getFullYear()}. All rights reserved.</small>   
-        <h6 className="mb-0">Powered by <a to="https://www.mirjy.com" style={{textDecoration: "none"}}>Mirjy Technologies Ltd</a></h6>
+        <h6 className="mb-3">© {new Date().getFullYear()}. All rights reserved, <a href="https://www.mirjy.com" style={{textDecoration: "none", color: "#008080"}}>Mirjy Technologies Ltd</a>.</h6>   
       </div>
     </footer>
   )
